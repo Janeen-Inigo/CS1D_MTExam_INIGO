db58ca8ad70277f7c57c1b7939ef4d8cda37d6cc
Janeen-Inigo <inigojaneen01@gmail.com>
2025-03-21 20:04:00 +0800

    Add files via upload

---
+++ classSubject.js
@@ -0,0 +1,12 @@
+//Prompt
+let subject_title=prompt("Enter your favorite subject: "); //ask your favorite sub
+let class_sched= prompt("Enter your Class Schedule: ");  //ask your schedule 
+let classroom= prompt("Enter your Classroom: "); //ask the room
+let class_instructor= prompt("Enter your Instructor: "); //ask to enter Instructor 
+let student_name= prompt("Enter your Name: ");  //Your name
+
+//output
+console.log(`${student_name} is currently enrolled in ${subject_title} \nwith a class schedule of ${class_sched} at room ${classroom}. \nThe instructor for the subject is ${class_instructor}.`);
+
+
+
